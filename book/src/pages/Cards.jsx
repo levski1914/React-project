a2c20f3b303014c8792c4d118051c44c738638cd
Georgi Dimitrov <austria1233@abv.bg>
2024-07-24 21:31:04 +0300

    comment system

--- book/src/pages/Cards.jsx
+++ book/src/pages/Cards.jsx
@@ -9,6 +9,11 @@ const Cards = ({ book, addToWishlist, getImage }) => {
   const [dimensions, setDimensions] = useState({ width: 0, height: 0 });
   const [mousePosition, setMousePosition] = useState({ mouseX: 0, mouseY: 0 });
 
+  const [showComments, setShowComments] = useState(false);
+  const [comments, setComments] = useState([]);
+  const [newComment, setNewComment] = useState("");
+  const [likes, setLikes] = useState({});
+
   useEffect(() => {
     const card = cardRef.current;
     const updateDimensions = () => {
@@ -34,6 +39,32 @@ const Cards = ({ book, addToWishlist, getImage }) => {
     setMousePosition({ mouseX: 0, mouseY: 0 });
   };
 
+  const toggleComments = () => {
+    setShowComments(!showComments);
+  };
+
+  const handleCommentsSubmit = (e) => {
+    e.preventDefault();
+    if (newComment.trim() === "") return;
+    const comment = {
+      id: comments.length + 1,
+      user: currentUser ? currentUser.name : "User",
+      text: newComment,
+      timestamp: new Date(),
+      likes: 0,
+    };
+
+    setComments([...comments, comment]);
+    setNewComment("");
+  };
+
+  const handleLike = (commentId) => {
+    setLikes((prevLikes) => ({
+      ...prevLikes,
+      [commentId]: (prevLikes[commentId] || 0) + 1,
+    }));
+  };
+
   const { width, height } = dimensions;
   const { mouseX, mouseY } = mousePosition;
 
@@ -50,7 +81,7 @@ const Cards = ({ book, addToWishlist, getImage }) => {
   };
 
   return (
-    <div className="card-container">
+    <div className={`card-container ${showComments ? "expanded" : ""}`}>
       <div
         className="card"
         ref={cardRef}
@@ -66,6 +97,51 @@ const Cards = ({ book, addToWishlist, getImage }) => {
         <span onClick={() => addToWishlist(book)} className="wishIcon">
           <FontAwesomeIcon icon="fa-regular fa-heart" />
         </span>
+        <button className="Show" onClick={toggleComments}>
+          <FontAwesomeIcon icon="fa-solid fa-comments" />
+        </button>
+
+        <div className={`comments ${showComments ? "show" : ""}`}>
+          <ul className="comment-list">
+            {comments.map(())
+
+            }
+            <li className="comment">
+              <div className="comment-header">
+                <h4>User</h4>
+                <h5>
+                  Posted: 2min{" "}
+                  <span>
+                    {" "}
+                    <FontAwesomeIcon icon="fa-regular fa-thumbs-up" />
+                  </span>{" "}
+                  <span>
+                    {" "}
+                    <FontAwesomeIcon icon="fa-regular fa-thumbs-up" />
+                  </span>{" "}
+                </h5>
+              </div>
+              <div className="comment-body">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et,
+                repellendus.
+              </div>
+              <div className="likes">
+                <ul className="icons">
+                  <li className="like-btn">
+                    <FontAwesomeIcon icon="fa-regular fa-thumbs-up" />
+                  </li>
+                  <li className="like-btn">
+                    <FontAwesomeIcon icon="fa-regular fa-thumbs-down" />
+                  </li>
+                </ul>
+              </div>
+            </li>
+          </ul>
+          <form className="send">
+            <input type="text" placeholder="Type your comment here" />
+            <button type="submit"> send</button>
+          </form>
+        </div>
       </div>
     </div>
   );

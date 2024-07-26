b995a16358eec3c25d5f71ac3eaac50805a3baac
Georgi Dimitrov <austria1233@abv.bg>
2024-07-26 18:48:50 +0300

    comment system fixed

--- book/src/Authcontext.jsx
+++ book/src/Authcontext.jsx
@@ -1,7 +1,8 @@
 // src/AuthContext.js
 import React, { createContext, useContext, useEffect, useState } from "react";
-import { auth } from "./firebase";
+import { auth, db } from "./firebase";
 import { onAuthStateChanged, signOut } from "firebase/auth";
+import { doc, getDoc } from "firebase/firestore";
 
 const AuthContext = createContext();
 
@@ -14,8 +15,13 @@ export const AuthProvider = ({ children }) => {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (user) => {
-      setCurrentUser(user);
+    const unsubscribe = onAuthStateChanged(auth, async (user) => {
+      if (user) {
+        const userDoc = await getDoc(doc(db, "users", user.uid));
+        setCurrentUser({ ...user, name: userDoc.data().name });
+      } else {
+        setCurrentUser(null);
+      }
       setLoading(false);
     });
 

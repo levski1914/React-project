bb1056432f0093328b89073c7421b06eebe16f1d
Georgi Dimitrov <austria1233@abv.bg>
2024-08-04 20:29:49 +0300

    some unusual files removed

--- book/src/pages/WishList.jsx
+++ book/src/pages/WishList.jsx
@@ -1,7 +1,7 @@
 // src/pages/Wishlist.js
 import React, { useState } from "react";
 // import { useWishlist } from "WishlistContext";
-
+import "./Styles/WishList.css";
 import { useWishlist } from "./WishContext";
 import Aside from "./Aside";
 const Wishlist = () => {
@@ -9,41 +9,47 @@ const Wishlist = () => {
 
   return (
     <>
-      <Aside />
-      <div className="wishList">
-        <h1>Wishlist</h1>
+      <div className="wishPage">
+        <Aside />
+        <div className="wishList">
+          <h1>Wishlist</h1>
 
-        <div
-          className="mainArea"
-          style={{ maxWidth: "1300px", margin: "0 auto", textAlign: "center" }}
-        >
-          {wishlist.length === 0 ? (
-            <p>No Products Added To The Wishlist</p>
-          ) : (
-            <table>
-              <thead>
-                <tr>
-                  <th>Product name</th>
-                  <th>Author</th>
-                  <th>Uploaded</th>
-                </tr>
-              </thead>
-              <tbody>
-                {wishlist.map((book) => (
-                  <tr key={book.id} style={{ border: "1px solid" }}>
-                    <td>{book.title}</td>
-                    <td>{book.author}</td>
-                    <td>{book.createdAt?.toDate().toDateString()}</td>
-                    <td>
-                      <button onClick={() => removeFromWishlist(book.id)}>
-                        remove
-                      </button>
-                    </td>
+          <div
+            className="mainArea"
+            style={{
+              maxWidth: "1300px",
+              margin: "0 auto",
+              textAlign: "center",
+            }}
+          >
+            {wishlist.length === 0 ? (
+              <p>No Products Added To The Wishlist</p>
+            ) : (
+              <table>
+                <thead>
+                  <tr>
+                    <th>Product name</th>
+                    <th>Author</th>
+                    <th>Uploaded</th>
                   </tr>
-                ))}
-              </tbody>
-            </table>
-          )}
+                </thead>
+                <tbody>
+                  {wishlist.map((book) => (
+                    <tr key={book.id} style={{ border: "1px solid" }}>
+                      <td>{book.title}</td>
+                      <td>{book.author}</td>
+                      <td>{book.createdAt?.toDate().toDateString()}</td>
+                      <td>
+                        <button onClick={() => removeFromWishlist(book.id)}>
+                          remove
+                        </button>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            )}
+          </div>
         </div>
       </div>
     </>
